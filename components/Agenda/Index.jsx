1d685655b1888d647485cdbdf9154348645ccd1a
imanehmz <ji_hamzaoui@esi.dz>
2022-09-30 21:45:13 +0100

    desktop version v1.2

--- components/Agenda/Index.jsx
+++ components/Agenda/Index.jsx
@@ -11,9 +11,9 @@ const Agenda = () => {
   return (
     <section>
       <SectionTitle title={"Agenda"}> </SectionTitle>
-      <div className="md:grid grid-cols-7 flex flex-col justify-center">
+      <div className="md:grid grid-cols-6 flex flex-col justify-center">
         <div className=" md:flex flex-col hidden md:visible">
-          <div className="md:visible invisible lg:ml-10 absolute pt-10 md:ml-0">
+          <div className="md:visible invisible lg:ml-[60px] absolute pt-10 md:ml-0">
             <Image
               src={globe}
               alt="globe"
@@ -26,33 +26,28 @@ const Agenda = () => {
             <Image
               src={atom}
               alt="atom"
-              width={80}
-              height={80}
+              width={90}
+              height={90}
               layout="fixed"
             />
           </div>
         </div>
 
-        <div className="col-span-5 flex flex-col justify-center ">
+        <div className="col-span-4 flex flex-col justify-center ">
           <YellowButton title={"Saturday 27th Sep"} />
-          <div className="justify-center bg-qiskit-blue-lighter w-full pl-[50px] mb-20 pr-[30px] py-[100px] md:py[80px] py-[50px] border-8 mt-[-30px]">
+          <div className="justify-center bg-qiskit-blue-lighter w-full md:pl-[50px] pl-[20px] mb-20 pr-[30px] lg:py-[100px] md:py[80px] py-12 border-4 md:border-8 mt-[-30px]">
             <div className="">
-              <div className="flex flex-row md:text-xl font-medium md:font-semibold sm:text-l py-0 md:px[10px]">
-                <div className="flex-none">
+              <div className="md:text-xl font-medium md:font-semibold sm:text-l py-0 md:px[10px] ">
+                <div className="flew flex-row">
                   {agenda.map((agendaInfo, key) => {
                     return (
-                      <div key={key} className="flex flex-col py-3">
-                        <div className="flex-none">{agendaInfo.Time}</div>
-                      </div>
-                    );
-                  })}
-                </div>
-
-                <div className="">
-                  {agenda.map((agendaInfo, key) => {
-                    return (
-                      <div key={key} className="flex flex-col py-3 px-5">
-                        <div className="">{agendaInfo.event}</div>
+                      <div className="flex flex-row justify-stretch mx-auto">
+                        <div key={key} className="flex flex-none py-3 lg:pl-10">
+                          <div className="">{agendaInfo.Time}</div>
+                        </div>
+                        <div key={key} className="flex pl-10 py-3 ">
+                            <div className="">{agendaInfo.event}</div>
+                          </div>
                       </div>
                     );
                   })}
@@ -62,7 +57,7 @@ const Agenda = () => {
           </div>
         </div>
         <div className=" md:flex flex-col hidden md:visible">
-          <div className="md:visible invisible absolute lg:ml-[20px] md:ml-0 mt-[-50px]">
+          <div className="md:visible invisible absolute lg:ml-[30px] md:ml-5 mt-[-50px]">
             <Image
               src={maqam}
               alt="maqam chahid"
@@ -71,7 +66,7 @@ const Agenda = () => {
               layout="fixed"
             />
           </div>
-          <div className="md:visible invisible  absolute lg:ml-[80px] md:ml-[20px] mt-[200px]">
+          <div className="md:visible invisible  absolute lg:ml-[120px] md:ml-[40px] mt-[200px]">
             <Image
               src={globe}
               alt="globe"
@@ -80,7 +75,7 @@ const Agenda = () => {
               layout="fixed"
             />
           </div>
-          <div className="md:visible invisible absolute md:ml-[0px] mt-[400px]">
+          <div className="md:visible invisible absolute md:ml-[10px] mt-[400px]">
             <Image
               src={computer}
               alt="computer"
--- components/shared/YellowButton.jsx
+++ components/shared/YellowButton.jsx
@@ -3,7 +3,7 @@ const YellowButton = ({ title }) => {
     <div className="z-20 mb-[-20px]">
       <div className="bg-[url('/agenda/yellow_button.svg')] bg-no-repeat bg-center bg-contain z-10 mt-15">
         <div className="h-1/6 flex justify-center">
-          <div className="h-[100px] text-3xl pt-[30px] font-semibold">{title}</div>
+          <div className="md:h-[100px] h-[70px] md:text-3xl  text-2xl pt-[20px] md:pt-[30px]">{title}</div>
         </div>
       </div>
     </div>
