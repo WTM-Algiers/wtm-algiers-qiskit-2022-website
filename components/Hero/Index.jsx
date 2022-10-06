0d2ecf36f011835b93bae05109475fd2f9d46173
Brivan-26 <ma.rezazi@esi-sba.dz>
2022-10-06 12:02:21 +0100

    Fixed arrow dimensions on 2xl screens

--- components/Hero/Index.jsx
+++ components/Hero/Index.jsx
@@ -33,8 +33,8 @@ const Hero = () => {
                 Your chance to discover the Quantum Computing world!
               </p>
               <div className="flex items-end">
-                <div className="h-[67px] w-[67px]">
-                  <Image src={Arrow} />
+                <div className="relative h-[67px] w-[67px] 2xl:w-[101px] 2xl:h-[114px]">
+                  <Image src={Arrow} layout="fill"/>
                 </div>
                 <div className="mb-[-50px] 2xl:mb-[-110px]">
                   <Purpulebutton title={"Register Now!"}/>
--- components/shared/SectionTitle.jsx
+++ components/shared/SectionTitle.jsx
@@ -10,11 +10,11 @@ const SectionTitle = ({ isWhite = false, title }) => {
     >
       <div className="flex md:h-20 md:w-16 2xl:w-28 2xl:h-32">
         {title === "About us" ? (
-          <div className="relative w-[68px] h-[75px]">
+          <div className="relative w-[68px] h-[75px] 2xl:w-[113px] 2xl:h-[127px]">
             <Image src={upArrow} alt="" layout="fill" />
           </div>
         ) : (
-          <div className="relative w-[68px] h-[75px]">
+          <div className="relative w-[68px] h-[75px] 2xl:w-[113px] 2xl:h-[127px]">
             <Image src={downArrow} alt="" />
           </div>
         )}
