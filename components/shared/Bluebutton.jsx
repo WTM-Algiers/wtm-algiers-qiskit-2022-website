0b3b2a7a2d41f225e69c6fd8d3ae150ff2877bc4
Brivan-26 <ma.rezazi@esi-sba.dz>
2022-10-12 14:58:07 +0100

    Updated Registration Link

--- components/shared/Bluebutton.jsx
+++ components/shared/Bluebutton.jsx
@@ -1,15 +1,19 @@
 const Bluebutton = ({ title }) => {
-    return (
-      <a href="https://ivlv.me/yOJ7h" target="__blank" className="z-20 mb-[-20px]">
-        <div className="bg-[url('/hero/blue_button.svg')] bg-no-repeat bg-center bg-cover z-10">
-          <div className="h-1/6 mx-[25px] pb-2 flex justify-center">
-            <div className="sm:h-[100px] h-[70px] sm:text-3xl sm:font-medium text-l pt-[20px] sm:pt-[30px]">{title}</div>
+  return (
+    <a
+      href="https://forms.gle/gkWs1mjufLsHhPuX9"
+      target="__blank"
+      className="z-20 mb-[-20px]"
+    >
+      <div className="bg-[url('/hero/blue_button.svg')] bg-no-repeat bg-center bg-cover z-10">
+        <div className="h-1/6 mx-[25px] pb-2 flex justify-center">
+          <div className="sm:h-[100px] h-[70px] sm:text-3xl sm:font-medium text-l pt-[20px] sm:pt-[30px]">
+            {title}
           </div>
         </div>
-      </a>
-    );
-  };
-  
-  export default Bluebutton;
-  
- 
+      </div>
+    </a>
+  );
+};
+
+export default Bluebutton;
--- components/shared/Purpulebutton.jsx
+++ components/shared/Purpulebutton.jsx
@@ -1,13 +1,19 @@
 const Button = ({ title }) => {
-    return (
-      <a href="https://ivlv.me/yOJ7h" target="__blank" className="z-2">
-        <div className="bg-[url('/hero/purpule_button.svg')] bg-no-repeat bg-center bg-contain z-10">
-          <div className=" place-content-center flex justify-center">
-            <div className="h-[90px] md:h-[110px] 2xl:h-[200px] mx-[40px] md:mx-[40px] 2xl:mx-[60px] sm:font-medium text-qiskit-white text-[17px]  2xl:text-[24px] pt-[30px] md:pt-[40px] 2xl:pt-[80px] sm:pt-[30px]">{title}</div>
+  return (
+    <a
+      href="https://forms.gle/gkWs1mjufLsHhPuX9"
+      target="__blank"
+      className="z-2"
+    >
+      <div className="bg-[url('/hero/purpule_button.svg')] bg-no-repeat bg-center bg-contain z-10">
+        <div className=" place-content-center flex justify-center">
+          <div className="h-[90px] md:h-[110px] 2xl:h-[200px] mx-[40px] md:mx-[40px] 2xl:mx-[60px] sm:font-medium text-qiskit-white text-[17px]  2xl:text-[24px] pt-[30px] md:pt-[40px] 2xl:pt-[80px] sm:pt-[30px]">
+            {title}
           </div>
         </div>
-      </a>
-    );
-  };
-  
-  export default Button;
+      </div>
+    </a>
+  );
+};
+
+export default Button;
